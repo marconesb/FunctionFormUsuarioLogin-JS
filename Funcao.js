5c65214c36498d9d46caa555ed838bb4298efdf1
Marcone Silva de Brito <marconebritt@gmail.com>
2022-06-14 22:05:41 -0300

    Initial Commit

---
+++ Funcao.js
@@ -0,0 +1,12 @@
+
+
+
+ // Função para formulário
+
+
+function bemVindo(){  
+const nome = document.querySelector('#nomeUsuario').value;
+alert(`Olá ${nome}! Seja bem-vindo!`);
+document.getElementById('formularioLogin').style.display = 'none';             
+}
+
